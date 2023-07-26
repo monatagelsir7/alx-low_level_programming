a88639b7cf0daf441d0a0a3433a5cc3840109475
monatagelsir7 <monatagelsir777@gmail.com>
2023-07-26 16:50:41 -0700

    function that encodes a string into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+/**
+ * leet - Encode a string
+ * @str: The string to be encoded
+ *
+ * Return: A pointer to the modified string
+ */
+char *leet(char *str)
+{
+	int index1 = 0, index2;
+	char leet[8] = {'O', 'L', '_', 'E', 'A', '_', '_', 'T'};
+
+	while (str[index1])
+	{
+
+		for (index2 = 0; index2 <= 7; index2++)
+		{
+			if (str[index1] == leet[index2] ||
+			    str[index1] - 32 == leet[index2])
+			{
+				str[index1] = index2 + '0';
+			}
+		}
+
+		index1++;
+	}
+	return (str);
+}
