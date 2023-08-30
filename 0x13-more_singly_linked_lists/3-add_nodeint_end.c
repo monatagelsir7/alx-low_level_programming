60545a62849a3cc6a6860c49014893c9a3e45908
monatagelsir7 <monatagelsir777@gmail.com>
2023-08-29 22:55:43 -0700

    a function that adds a new node at the end of a listint_t list

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,35 +1,34 @@
 #include "lists.h"
-
+#include <stdlib.h>
 /**
- * add_nodeint_end - Add a new node at the end of a listint_t list
- * @head: A pointer to a pointer to the head of the linked list
- * @n: The integer to put inside the new node
- *
- * Return: The address of the new element, or NULL if it failed
- */
+  * add_nodeint_end - add node at end of a listint_t list.
+  *
+  * @head: head of double pointer
+  * @n: int add the list
+  * Return: NULL if it failed
+  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new;
-	listint_t *tail = *head;
+	listint_t *temp;
 
-	new = malloc(sizeof(listint_t));
-
-	if (!new)
+	if (head == NULL)
 		return (NULL);
-
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
 	new->n = n;
 	new->next = NULL;
-
-	if (*head  == NULL)
+	if (*head == NULL)
 	{
-		*head = new;
+	*head = new;
 		return (new);
 	}
-
-	while (tail->next)
-		tail = tail->next;
-
-	tail->next = new;
-
+	temp = *head;
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+	temp->next = new;
 	return (new);
 }
